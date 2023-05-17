aa5efb7d520adab83b4a839e5a76ca976b94b531
Vika <la.clem6m@gmail.com>
2023-05-17 20:58:21 +0300

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,26 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('server', function() {
-
-    browserSync({
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
         server: {
             baseDir: "src"
         }
     });
-
-    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+gulp.task('sass-watch', function() {
+    return gulp.src("srs/sass/**/*.+(sass|scss)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+        .pipe(browserSync.stream()); 
 });
 
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+gulp.tack('watch', function() {
+    gulp.watch('srs/sass/**/*.+(sass|scss)', gulp.parallel ('browserSync'));
+    gulp.watch('src/*.html'). on('change', browserSync.reload)
+});
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
